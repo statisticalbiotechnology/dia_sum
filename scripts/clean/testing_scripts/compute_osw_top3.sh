46526f386d2fcc7e0a4c83dcb51b046d4b50426a
patruong <ptruong@kth.se>
2022-03-14 13:00:33 +0100

    upd

---
+++ scripts/clean/testing_scripts/compute_osw_top3.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+python ../top3.py --input /hdd_14T/data/PXD002952/20210614_dataset/result_files_20220214/DID/osw_top3_input_formatted.csv --output top3_output_osw.csv
--- scripts/clean/top3.py
+++ scripts/clean/top3.py
@@ -11,12 +11,9 @@ import os
 import pandas as pd
 import numpy as np
 import scipy.stats as stats
-
-#from parsers.parse_triqler import parse_triqler
 from q_value import qvalues
-#from mappers import experiment_id_mapper, sample_id_mapper, specie_mapper, decoy_mapper
-#import top3_formatter.diann
-#import top3_formatter.osw
+import argparse
+
 
 def top3(df):
     A = df[df.iloc[:, df.columns.get_level_values("sample_id") == "1"].isna().sum(axis=1)<2]
@@ -24,18 +21,22 @@ def top3(df):
     B = df[df.iloc[:, df.columns.get_level_values("sample_id") == "2"].isna().sum(axis=1)<2]
     B = B.iloc[:,B.columns.get_level_values("sample_id") == "2"]
     
+    print("Finding all overlapping proteins in Sample A and Sample B.")
     # Find overlapping proteins
     overlapping_proteins = list(set(A.index) & set(B.index))
     A = A[A.index.isin(overlapping_proteins)]
     B = B[B.index.isin(overlapping_proteins)]
     
+    print("Computing p-values using scipy.stats.ttest_ind() between sample A and B.")
     p_vals = stats.ttest_ind(A, B, axis = 1)[1]
     p_vals = pd.DataFrame(p_vals, columns = ["p"])
+    print("Computing q-values.")
     p_vals["q"] = qvalues(p_vals)
     p_vals = pd.DataFrame(p_vals.values, index = A.index, columns = ["p", "q"])
     p_vals.sort_values("q",inplace =True)
     p_vals = p_vals.astype(float)
     
+    print("Applying log2-transformation to sample sums")
     A = np.log2(A.sum(axis=1))
     B = np.log2(B.sum(axis=1))
     
@@ -43,29 +44,35 @@ def top3(df):
     B.name = "2"
     
     df_final = pd.concat([A, B, p_vals], axis = 1)
+    print("Computing fold-change")
     df_final["log2(A,B)"] = df_final["1"] - df_final["2"]
     return df_final
 
+parser = argparse.ArgumentParser(
+    description='This script takes input from top3_formatter/diann.py and top3_formatter/osw.py and computes the protein quantity, p-values and q-values.',
+    formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+
+parser.add_argument('--input', type=str,
+                    help='Top3 input file. This file be generated using top3_formatter/diann.py and top3_formatter/osw.pyl')
+
+parser.add_argument('--output', type=str, default = "top3_output.csv",
+                    help='Name of the output file.')
 
 
+# parse arguments from command line
+args = parser.parse_args()
+input_file = args.input
+output = args.output
 
-
-# DIANN
-df = pd.read_csv("report_for_top3.tsv", sep = "\t")
-#triq = pd.read_csv("triqler_input_diann_searchScore_Qvalue.csv", sep = "\t")                      
-fdr_treshold = 0.01
-df = df[df["Q.Value"] < fdr_treshold]
-df = top3_formatter.diann.avg_3_largest_precursor(df)
-df = top3(df)
-
-
-# OSW
-df = top3_formatter.osw.avg_3_largest_precursor("osw_results", m_score_treshold = 0.01)
-df = top3(df)
-
-
-
-
+if __name__ == "__main__":
+    print("Reading file: " + input_file)
+    input_df = pd.read_csv(input_file, sep = "\t", index_col = [0,1], header = [0,1])
+    print("Starting top3 computation...")
+    res_df = top3(input_df)
+    print("Printing output: " + output)
+    res_df.to_csv(output, sep = "\t")
+    print("Done!")
+    
 
 
 
