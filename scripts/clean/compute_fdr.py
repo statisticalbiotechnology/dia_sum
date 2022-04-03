c0cbba375b960d658d19f063ed21574f4da1fc58
patruong <ptruong@kth.se>
2022-04-03 07:58:54 +0200

    added diann converters

---
+++ scripts/clean/compute_fdr.py
@@ -0,0 +1,87 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Apr  2 21:01:58 2022
+
+@author: ptruong
+"""
+import pandas as pd 
+import numpy as np
+import numpy.random as npr
+import argparse
+
+def bootstrap(invec):
+    idx = npr.randint(0, len(invec), len(invec))
+    return [invec[i] for i in idx]
+
+def estimatePi0(p, numBoot=100, numLambda=100, maxLambda=0.95):
+    p.sort()
+    n=len(p)
+    lambdas=np.linspace(maxLambda/numLambda,maxLambda,numLambda)
+    Wls=np.array([n-np.argmax(p>=l) for l in lambdas])
+    pi0s=np.array([Wls[i] / (n * (1 - lambdas[i])) for i in range(numLambda)])
+    minPi0=np.min(pi0s)
+    mse = np.zeros(numLambda)
+    for boot in range(numBoot):
+        pBoot = bootstrap(p)
+        pBoot.sort()
+        WlsBoot =np.array([n-np.argmax(pBoot>=l) for l in lambdas])
+        pi0sBoot =np.array([WlsBoot[i] / (n *(1 - lambdas[i])) for i in range(numLambda)])
+        mse = mse + np.square(pi0sBoot-minPi0)
+    minIx = np.argmin(mse)
+    return pi0s[minIx]
+
+def qvalues(pvalues, pcol = "p"):
+    m = pvalues.shape[0] # The number of p-values
+    pvalues.sort_values(pcol,inplace=True) # sort the pvalues in acending order
+    pi0 = estimatePi0(list(pvalues[pcol].values))
+    print("pi_0 estimated to " + str(pi0))
+    
+    # calculate a FDR(t) as in Storey & Tibshirani
+    num_p = 0.0
+    for ix in pvalues.index:
+        num_p += 1.0
+        fdr = pi0*pvalues.loc[ix,pcol]*m/num_p
+        pvalues.loc[ix,"q"] = fdr
+    
+    # calculate a q(p) as the minimal FDR(t)
+    old_q=1.0
+    for ix in reversed(list(pvalues.index)):
+        q = min(old_q,pvalues.loc[ix,"q"])
+        old_q = q
+        pvalues.loc[ix,"q"] = q
+    return pvalues
+
+
+def recompute_q_value(filename = "report.tsv", output = "report_recomputed_fdr.tsv", pcol = "Global.Q.Value"):
+    df = pd.read_csv(filename,sep="\t")    
+    df = qvalues(df, pcol = pcol)
+    df.to_csv(output, sep = "\t", index = False)
+    
+parser = argparse.ArgumentParser(
+    description='Recomputes the FDR .',
+    formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+
+parser.add_argument('--input', type=str,
+                    help='Input file to compute fdr.')
+
+parser.add_argument('--output', type=str,
+                    help='Output name.')
+
+parser.add_argument('--pcol', type=str,
+                    help='p-value or similar metric columns to be used to recompute the FDR.')
+
+# parse arguments from command line
+args = parser.parse_args()
+input_file = args.input
+output = args.output
+pcol = args.pcol
+
+if __name__ == "__main__":
+    recompute_q_value(filename = input_file, output = output, pcol = pcol)
+    
+
+
+
+
+
---
+++ scripts/clean/convert_diann_to_msstats.py
@@ -0,0 +1,151 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Aug 12 10:27:31 2021
+
+@author: ptruong
+"""
+
+import os 
+
+import pandas as pd
+import numpy as np
+import argparse
+
+
+def convert_diann_to_msconvert_aggregated(df):
+    df_ = pd.DataFrame()    
+    df_["ProteinName"] = df["Protein.Ids"]
+    df_["PeptideSequence"] = df["Modified.Sequence"]
+    df_["PrecursorCharge"] = df["Precursor.Charge"]
+    df_["FragmentIon"] = df["Fragment.Info"]
+    df_["ProductCharge"] = np.nan
+    df_["IsotopeLabelType"] = "light"
+    df_["Intensity"] = df["Fragment.Quant.Raw"] 
+    
+    bioRep_mapper = lambda x: x.split("Repl")[-1]
+    condition_mapper = lambda x: x.split("_")[8]
+    run_mapper = lambda x: x.split("_")[5]
+    
+    df_["BioReplicate"] = df.Run.map(bioRep_mapper)
+    df_["Condition"] = df.Run.map(condition_mapper)
+    df_["Run"] = df.Run.map(run_mapper)
+    return df_
+
+# Count and filter based on fragment ion 5-15
+
+def disaggregate(df, fragment_info_col = "FragmentIon", fragment_quant_col = "Intensity", seperator = ";"):
+    col_fragment_info = fragment_info_col
+    col_fragment_quant = fragment_quant_col
+    
+    fragment_info = df[col_fragment_info].str.split(seperator).apply(pd.Series, 1).stack()
+    fragment_info.name = col_fragment_info
+    fragment_info.index = fragment_info.index.droplevel(-1)
+    fragment_info = fragment_info[fragment_info != ""]
+    
+    df = df.drop(col_fragment_info, axis = 1)
+    
+    fragment_quant = df[col_fragment_quant].str.split(seperator).apply(pd.Series, 1).stack()
+    fragment_quant.name = col_fragment_quant
+    fragment_quant.index = fragment_quant.index.droplevel(-1)
+    fragment_quant = fragment_quant[fragment_quant != ""]
+    
+    df = df.drop(col_fragment_quant, axis = 1)
+    
+    fragment_quant = pd.DataFrame(fragment_quant)
+    fragment_info = pd.DataFrame(fragment_info)
+    
+    fragment_quant = fragment_quant.reset_index()
+    fragment_info = fragment_info.reset_index().drop("index", axis = 1)
+    
+    fragment_quant[fragment_info_col] = fragment_info
+    fragment_quant.set_index("index", inplace=True)
+    
+    df = df.reset_index()
+    df.set_index("index", inplace=True)
+    
+    df=df.join(fragment_quant)
+    df[fragment_quant_col] = df[fragment_quant_col].astype(float)
+    return df
+
+def filter_n_fragments(df, min_fragments = 4, max_fragments = 6, aggr_fragment_col = "FragmentIon"):
+    df["n_fragments"] = df[aggr_fragment_col].str.split(";").apply(len)-1
+    df = df[df.n_fragments <= max_fragments]
+    df = df[df.n_fragments >= min_fragments]
+    df = df.drop("n_fragments", axis = 1)
+    return df
+
+
+def filter_on_min_peptide(df, n_peptides):
+    peptide_count = df.groupby("PeptideSequence").count().ProteinName
+    min_peptide = peptide_count[peptide_count >= n_peptides] # greater than
+    df = df[df.PeptideSequence.isin(min_peptide.index)]
+    return  df
+
+def filter_on_max_peptide(df, n_peptides):
+    peptide_count = df.groupby("PeptideSequence").count().ProteinName
+    max_peptide = peptide_count[peptide_count <= 10] # less than
+    df = df[df.PeptideSequence.isin(max_peptide.index)]
+    return df
+
+def drop_decoy_proteins(df):
+    df["Decoy"] = (df.ProteinName.map(lambda x:x.split("_")[0]) == "DECOY")
+    df = df[df.Decoy != True]
+    df.drop("Decoy", inplace = True, axis = 1)
+    return df
+
+
+
+def convert_diann_to_msstats(input_file = "report_recomputed_fdr.tsv", output = "msstats_input.csv", fdr_threshold = 0.01, qCol = "q"):
+    print("Converting diann to MSstats")
+    print(f"fdr threshold {fdr_threshold}")
+    df = pd.read_csv(input_file, sep = "\t")
+    df = df[df[qCol] < fdr_threshold] # filter PSMs Q.Value - 167463
+
+    df = convert_diann_to_msconvert_aggregated(df)
+    #df # 167463
+    df = filter_on_min_peptide(df, n_peptides = 2) # 166236
+    df = filter_on_max_peptide(df, n_peptides = 10) # 93395
+    df = filter_n_fragments(df, min_fragments = 1, max_fragments = 6, aggr_fragment_col = "FragmentIon")
+    df = drop_decoy_proteins(df)
+    
+    df = disaggregate(df, fragment_info_col = "FragmentIon", fragment_quant_col = "Intensity", seperator = ";")
+    
+    df.to_csv(output, sep = ",", index = False)
+    
+    
+
+parser = argparse.ArgumentParser(
+    description='Convert DIA-NN to MSstats input format.',
+    formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+
+parser.add_argument('--input_file', type=str,
+                    help='input file name.')
+
+parser.add_argument('--fdr_threshold', type=float, default = 0.01,
+                    help='fdr threshold to apply on qCol.')
+
+parser.add_argument('--qCol', type=str, default = "q",
+                    help='q value column to use.')
+
+parser.add_argument('--output', type=str, default = "msstats_input.csv",
+                    help='output name.')
+
+# parse arguments from command line
+args = parser.parse_args()
+input_file = args.input_file
+fdr_threshold = args.fdr_threshold
+qCol = args.qCol
+output = args.output
+
+
+if __name__ == "__main__":
+    convert_diann_to_msstats(input_file, output, fdr_threshold, qCol, output)
+
+
+
+
+
+
+
+
