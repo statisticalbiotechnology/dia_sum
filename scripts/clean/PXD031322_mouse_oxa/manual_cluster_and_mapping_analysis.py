0521ba7784f640f8cfbec5837a75acca1b1efd79
patruong <ptruong@kth.se>
2022-09-01 17:24:16 +0200

    update pathway analysis

--- scripts/clean/PXD031322_mouse_oxa/manual_cluster_and_mapping_analysis.py
+++ scripts/clean/PXD031322_mouse_oxa/manual_cluster_and_mapping_analysis.py
@@ -14,10 +14,40 @@ ToDo:
     Continue on gprofiler background
     Plot gprofiler KEGG pathways as in paper
 """
+
 import pandas as pd 
 import numpy as np
+import gseapy as gp
+import os
+os.chdir("/home/ptruong/git/dia_sum/scripts/clean/PXD031322_mouse_oxa")
 from uniprot_idmapper import *
+from gseapy_plot import *
 
+os.chdir("/hdd_14T/data/PXD031322_oxaliplatin_dia_study/ftp.pride.ebi.ac.uk/pride/data/archive/2022/07/PXD031322/2022-08-11_run")
+
+
+def parse_triqler(triqler_output_file):
+    """
+    Parses triqler output format to pandas dataframe.
+    """
+    f = open(triqler_output_file, "r")
+    lines = f.readlines()
+    line = lines.pop(0)
+    cols = line.split("\n")[0].split("\t")[:]
+    n_cols = len(cols)
+    
+    data_array = []
+    for line in lines:
+        line = line.split("\n")[0].split("\t")
+        vals = line[:n_cols-1]
+        peptides = ";".join(line[n_cols-1:])
+        data = vals + [peptides]
+        data_array.append(data)
+    df = pd.DataFrame(data_array, columns = cols)
+
+    df = pd.concat([df[["protein", "peptides"]], df.drop(["protein", "peptides"], axis = 1).astype(float)], axis = 1)
+    
+    return df
 
 def get_clusters(ctrl_lt_file = "proteins.1vs2.tsv",
                  ctrl_st_file = "proteins.1vs3.tsv",
@@ -100,26 +130,133 @@ def get_mapped_proteins(ids):
                  index = ["primaryAccession", "secondaryAccession", "uniProtKB_ID", "geneName", "geneNameSynonyms"]).T
     return res    
 
+def qprofiler_run(query_genes, background_genes, user_threshold = 0.05):
+    import requests
+    r = requests.post(
+        url='https://biit.cs.ut.ee/gprofiler/api/gost/profile/',
+        json={
+        'organism':'mmusculus',
+        'query':query_genes,
+        'sources' :['KEGG'], #only look into Gene Ontology terms.
+        'user_threshold':user_threshold, #reduce the significance threshold,
+        'significance_threshold_method':'bonferroni', #use bonferroni correction instrad of the default 'g_SCS'.
+        'no_evidences':True, #skip lookup for evidence codes. Speeds up queries, if there is no interest in evidence codes.
+        'no_iea':True, #Ignore electonically annotated GO annotations
+        'domain_scope':'custom',#use the genes in the probe as the statistical background.
+        'background':background_genes
+        },
+        headers={
+        'User-Agent':'FullPythonRequest'
+        }
+    )
+    
+    res = r.json()['result']
+    return res
+
+def plot_dotplot(df, size = 10, title = "KEGG"):
+    temp = df["Overlap"].str.split("/", expand=True).astype(int)
+    df = df.assign(Hits_ratio=temp.iloc[:, 0] / temp.iloc[:, 1])
+    # make area bigger to better visualization
+    # area = df["Hits_ratio"] * plt.rcParams["lines.linewidth"] * 100
+    area = np.pi * (df["Hits_ratio"] * size * plt.rcParams["lines.linewidth"]).pow(2)
+    
+    xlabel = "Cluster"
+    
+    x = df["group"]
+    ylabels = df["Term"].values
+    cbar_title = r"Adjusted P-value"
+    figsize: Tuple[float] = (6, 5.5)
+    fig, ax = plt.subplots(figsize=figsize)
+    colname = "Adjusted P-value"
+    #colmap = df[colname].round().astype("int")
+    colmap = df[colname]
+    vmin = np.percentile(colmap.min(), 2)
+    vmax = np.percentile(colmap.max(), 98)
+    #cmap: str = "viridis_r"
+    cmap: str = "viridis"
+    sc = ax.scatter(
+        x=x,
+        y=ylabels,
+        s=area,
+        edgecolors="face",
+        c=colmap,
+        cmap=cmap,
+        vmin=vmin,
+        vmax=vmax,
+    )
+    
+    
+    ax.set_xlabel(xlabel, fontsize=14, fontweight="bold")
+    ax.xaxis.set_tick_params(labelsize=14)
+    ax.yaxis.set_tick_params(labelsize=16)
+    ax.set_axisbelow(True)  # set grid blew other element
+    ax.grid(axis="y")  # zorder=-1.0
+    ax.margins(x=0.25)
+    
+    # We change the fontsize of minor ticks label
+    # ax.tick_params(axis='y', which='major', labelsize=16)
+    # ax.tick_params(axis='both', which='minor', labelsize=14)
+    
+    # scatter size legend
+    # we use the *func* argument to supply the inverse of the function
+    # used to calculate the sizes from above. The *fmt* ensures to string you want
+    handles, labels = sc.legend_elements(
+        prop="sizes",
+        num=3,  # fmt="$ {x:.2f}",
+        color="gray",
+        func=lambda s: np.sqrt(s / np.pi) / plt.rcParams["lines.linewidth"] / size,
+    )
+    ax.legend(
+        handles,
+        labels,
+        title="% Path\nis DEG",
+        bbox_to_anchor=(1.02, 0.9),
+        loc="upper left",
+        frameon=False,
+    )
+    # colorbar
+    # cax = fig.add_axes([1.0, 0.20, 0.03, 0.22])
+    cbar = fig.colorbar(
+        sc,
+        shrink=0.2,
+        aspect=10,
+        anchor=(0.0, 0.2),  # (0.0, 0.2),
+        location="right"
+        # cax=cax,
+    )
+    # cbar.ax.tick_params(right=True)
+    cbar.ax.set_title(cbar_title, loc="left", fontweight="bold")
+    for key, spine in cbar.ax.spines.items():
+        spine.set_visible(False)
+    
+    ax.set_title(title, fontsize=20, fontweight="bold")
+
 ctrl_lt = parse_triqler("proteins.1vs2.tsv").set_index("protein")
 ctrl_st = parse_triqler("proteins.1vs3.tsv").set_index("protein")
 lt_st = parse_triqler("proteins.2vs3.tsv").set_index("protein")
-
+C1, C2, C3, C4, C5, C6 = get_clusters(ctrl_lt_file = "proteins.1vs2.tsv",
+                                      ctrl_st_file = "proteins.1vs3.tsv",
+                                      lt_st_file = "proteins.2vs3.tsv")
 
 fdr_threshold = 0.05
 # Check ctrl-lt C1,C2
+c1_bgr = ctrl_lt[C1]
 c1 = ctrl_lt[C1 & (ctrl_lt.q_value < fdr_threshold)]
+c2_bgr = ctrl_lt[C2]
 c2 = ctrl_lt[C2 & (ctrl_lt.q_value < fdr_threshold)]
 
 # Check lt-st C3, c4
-c3 = lt_st[C1 & (lt_st.q_value < fdr_threshold)]
-c4 = lt_st[C2 & (lt_st.q_value < fdr_threshold)]
+c3_bgr = lt_st[C3]
+c3 = lt_st[C3 & (lt_st.q_value < fdr_threshold)]
+c4_bgr = lt_st[C4]
+c4 = lt_st[C4 & (lt_st.q_value < fdr_threshold)]
 
 # Check ctrl-st C5, C6
-c5 = ctrl_lt[C1 & (ctrl_lt.q_value < fdr_threshold)]
-c6 = ctrl_lt[C2 & (ctrl_lt.q_value < fdr_threshold)]
+c5_bgr = ctrl_lt[C5]
+c5 = ctrl_lt[C5 & (ctrl_lt.q_value < fdr_threshold)]
+c6_bgr = ctrl_lt[C6] 
+c6 = ctrl_lt[C6 & (ctrl_lt.q_value < fdr_threshold)]
 
-
-c1.index
 
 c1_mapped = get_mapped_proteins(ids = list(c1.index))
 c2_mapped = get_mapped_proteins(ids = list(c2.index))
@@ -128,37 +265,106 @@ c4_mapped = get_mapped_proteins(ids = list(c4.index))
 c5_mapped = get_mapped_proteins(ids = list(c5.index))
 c6_mapped = get_mapped_proteins(ids = list(c6.index))
 
+c1_bgr_mapped = get_mapped_proteins(ids = list(c1_bgr.index))
+c2_bgr_mapped = get_mapped_proteins(ids = list(c2_bgr.index))
+c3_bgr_mapped = get_mapped_proteins(ids = list(c3_bgr.index))
+c4_bgr_mapped = get_mapped_proteins(ids = list(c4_bgr.index))
+c5_bgr_mapped = get_mapped_proteins(ids = list(c5_bgr.index))
+c6_bgr_mapped = get_mapped_proteins(ids = list(c6_bgr.index))
 
-c1_mapped
-c2_mapped
-c3_mapped
-c4_mapped
-c5_mapped
-c6_mapped
 
-query_genes = list(c1_mapped.geneName)
+"""
 user_threshold = 0.05
-import requests
-r = requests.post(
-    url='https://biit.cs.ut.ee/gprofiler/api/gost/profile/',
-    json={
-    'organism':'mmusculus',
-    'query':query_genes,
-    'sources' :['KEGG'], #only look into Gene Ontology terms.
-    'user_threshold':user_threshold, #reduce the significance threshold,
-    'significance_threshold_method':'bonferroni', #use bonferroni correction instrad of the default 'g_SCS'.
-    'no_evidences':True, #skip lookup for evidence codes. Speeds up queries, if there is no interest in evidence codes.
-    'no_iea':True, #Ignore electonically annotated GO annotations
 
-    'domain_scope':'custom',#use the genes in the probe as the statistical background.
-    'background':'AFFY_HG_U133A'
-    },
-    headers={
-    'User-Agent':'FullPythonRequest'
-    }
-)
+c1_pathways = qprofiler_run(query_genes = list(c1_mapped.geneName),
+              background_genes = list(c1_bgr_mapped.geneName),
+              user_threshold = 0.05)
+c2_pathways = qprofiler_run(query_genes = list(c2_mapped.geneName),
+              background_genes = list(c2_bgr_mapped.geneName),
+              user_threshold = 0.05)
+c3_pathways = qprofiler_run(query_genes = list(c3_mapped.geneName),
+              background_genes = list(c3_bgr_mapped.geneName),
+              user_threshold = 0.05)
+c4_pathways = qprofiler_run(query_genes = list(c4_mapped.geneName),
+              background_genes = list(c4_bgr_mapped.geneName),
+              user_threshold = 0.05)
+c5_pathways = qprofiler_run(query_genes = list(c5_mapped.geneName),
+              background_genes = list(c5_bgr_mapped.geneName),
+              user_threshold = 0.05)
+c6_pathways = qprofiler_run(query_genes = list(c6_mapped.geneName),
+              background_genes = list(c6_bgr_mapped.geneName),
+              user_threshold = 0.05)
 
-obj = r.json()['result']
+"""
 
-type(obj[0])
 
+#gene_list = c1_mapped.geneName
+#background_list = c1_bgr_mapped.geneName
+mouse_library = gp.get_library_name(organism="Mouse")
+
+enr_c1 = gp.enrichr(gene_list=c1_mapped.geneName,
+                 gene_sets=['KEGG_2019_Mouse'],
+                 background=c1_bgr_mapped.geneName,
+                 organism='mouse', # don't forget to set organism to the one you desired! e.g. Yeast
+                 outdir=None, # don't write to disk
+                )
+enr_c2 = gp.enrichr(gene_list=c2_mapped.geneName,
+                 gene_sets=['KEGG_2019_Mouse'],
+                 background=c2_bgr_mapped.geneName,
+                 organism='mouse', # don't forget to set organism to the one you desired! e.g. Yeast
+                 outdir=None, # don't write to disk
+                )
+enr_c3 = gp.enrichr(gene_list=c3_mapped.geneName,
+                 gene_sets=['KEGG_2019_Mouse'],
+                 background=c3_bgr_mapped.geneName,
+                 organism='mouse', # don't forget to set organism to the one you desired! e.g. Yeast
+                 outdir=None, # don't write to disk
+                )
+enr_c4 = gp.enrichr(gene_list=c4_mapped.geneName,
+                 gene_sets=['KEGG_2019_Mouse'],
+                 background=c4_bgr_mapped.geneName,
+                 organism='mouse', # don't forget to set organism to the one you desired! e.g. Yeast
+                 outdir=None, # don't write to disk
+                )
+enr_c5 = gp.enrichr(gene_list=c5_mapped.geneName,
+                 gene_sets=['KEGG_2019_Mouse'],
+                 background=c5_bgr_mapped.geneName,
+                 organism='mouse', # don't forget to set organism to the one you desired! e.g. Yeast
+                 outdir=None, # don't write to disk
+                )
+enr_c6 = gp.enrichr(gene_list=c6_mapped.geneName,
+                 gene_sets=['KEGG_2019_Mouse'],
+                 background=c6_bgr_mapped.geneName,
+                 organism='mouse', # don't forget to set organism to the one you desired! e.g. Yeast
+                 outdir=None, # don't write to disk
+                )
+
+
+enr_c2.res2d["group"] = "C2"
+enr_c3.res2d["group"] = "C3"
+enr_c4.res2d["group"] = "C4"
+enr_c5.res2d["group"] = "C5"
+enr_c6.res2d["group"] = "C6"
+
+enr_clusters = pd.concat([enr_c1.res2d, enr_c2.res2d, enr_c3.res2d,enr_c4.res2d, enr_c5.res2d, enr_c6.res2d])
+enr_clusters["pathway_DEG"] = enr_clusters.Overlap.map(lambda x:float(x.split("/")[0]))
+enr_clusters["pathway_genes"] = enr_clusters.Overlap.map(lambda x:float(x.split("/")[1]))
+enr_clusters["% Path is DEG"] = enr_clusters["pathway_DEG"] / enr_clusters["pathway_genes"]
+
+paper_term = ["Ribosome", "Spliceosome", "Endocytosis", "Steroid biosynthesis",
+              "Dopaminergic synapse", "RNA transport", "Glutathione metabolism",
+              "Proteasome", "Tight junction", "Complement and coagulation cascades", 
+              "Metabolism of xenobiotics by cytochrome P450", "Fructose and mannose metabolism",
+              "mRNA surveillance pathway", "Arginine biosynthesis", "Protein export",
+              "Protein processing in endoplasmic reticulum", "N-Glycan biosynthesis",
+              "Tyrosine metabolism", "Metabolic pathways", "Adrenaergic signaling in cardiomyocytes"]
+
+plot_input = enr_clusters[enr_clusters["Adjusted P-value"] < 0.05]
+plot_input_paper_term = plot_input[plot_input.Term.isin(paper_term)]
+
+df = plot_input
+size = 10 # dot size baseline
+title="KEGG_2019_mouse" #title
+
+plot_dotplot(df = plot_input, size = 15, title = "KEGG_2019_mouse, fc_eval = 0.415, fdr = 0.05")
+plot_dotplot(df = plot_input_paper_term, size = 15, title = "KEGG_2019_mouse, fc_eval = 0.415, fdr = 0.05")
